f0c5fd75759fbe0c936f4d7cabaa7fc65cc58061
newbieJanghan <bjh4135@gmail.com>
2024-08-09 17:28:16 +0900

    Submit: SWEA_1289 (1/3)

--- day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
+++ day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
@@ -1,5 +1,23 @@
 package SWEA_1289_원재의메모리복구;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int t=1; t<=T; t++) {
+			int result = 0;
+			
+			char[] origin = sc.next().toCharArray();
+			char flag = '0';
+			for (char c: origin) {
+				if (flag != c) result++;
+				flag = c;
+			}
+			
+			System.out.println("#" + t + " " + result);
+		}
+	}
 }
