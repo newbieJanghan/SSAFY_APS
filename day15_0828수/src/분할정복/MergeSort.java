d751631c157a4b0f23b7cdb23f1ae82d400d8e20
newbieJanghan <bjh4135@gmail.com>
2024-08-28 14:30:13 +0900

    Implement mergeSort, quickSort

--- day15_0828수/src/분할정복/MergeSort.java
+++ day15_0828수/src/분할정복/MergeSort.java
@@ -1,45 +1,52 @@
 package 분할정복;
 
+import java.util.Arrays;
+
 public class MergeSort {
 	static int[] arr = { 7, 5, 13, 2, 79, 12, 35, 42 };
-	static int N = arr.length;
-	static int[] result = new int[N];
+	static int[] temp = new int[arr.length];
 
 	public static void main(String[] args) {
-
+		sort(0, arr.length - 1);
+		System.out.println(Arrays.toString(temp));
 	}
 
-	static void sort(int left, int right) {
-		if (left >= right) {
+	static void sort(int start, int end) {
+
+		if (start >= end) {
 			return;
 		}
 
-		int mid = left + right / 2;
-		sort(left, mid);
-		sort(mid + 1, right);
-		merge(left, mid, right);
+		int mid = (start + end) / 2;
+		sort(start, mid);
+		sort(mid + 1, end);
+		merge(start, mid, end);
 	}
 
 	static void merge(int start, int mid, int end) {
-		int left = start, right = mid + 1, index = start;
-		while (left <= mid && right <= end) {
-			if (arr[left] < arr[end]) {
-				result[index++] = arr[left++];
+		int L = start, R = mid + 1, index = start;
+
+		while (L <= mid && R <= end) {
+			if (arr[L] < arr[R]) {
+				temp[index++] = arr[L++];
 			} else {
-				result[index++] = arr[end++];
+				temp[index++] = arr[R++];
 			}
 		}
 
-		if (left <= mid) {
-			while (left <= mid) {
-				result[index++] = arr[left++];
+		if (L <= mid) {
+			while (L <= mid) {
+				temp[index++] = arr[L++];
 			}
 		} else {
-			while (right <= end) {
-				result[index++] = arr[right++];
+			while (R <= end) {
+				temp[index++] = arr[R++];
 			}
 		}
 		
-		
+		for (int i = start; i <= end; i++) {
+			arr[i] = temp[i];
+		}
+
 	}
 }
